3eb43bd10adf3eacde5e85ecca69b11622b7643a
thebatmanyo <62774470+thebatmanyo@users.noreply.github.com>
2021-11-14 02:22:12 -0500

    The Dance Floor Update

--- Assets/Scripts/DiscoLights.cs
+++ Assets/Scripts/DiscoLights.cs
@@ -7,18 +7,19 @@ public class DiscoLights : MonoBehaviour
     public Vector3 center;
     [SerializeReference]
     public float rotationMult;
+    public float spotAngleMult;
+    public GameObject baseLightLocation;
     private Light light;
 
     private void Start()
     {
-        center = transform.position;
+        center = baseLightLocation.transform.position;
         light = GetComponent<Light>();
 
     }
     void Update()
     {
         transform.RotateAround(center, Vector3.up, 135 * Time.deltaTime * rotationMult);
-        // transform.RotateAround(center, Vector3.right, 75 * Time.deltaTime);
-        light.spotAngle = (15f * Mathf.Sin(Time.time * 3)) + 65f;
+        light.spotAngle = (15f * Mathf.Sin(Time.time * 3) * spotAngleMult) + 45f;
     }
 }
